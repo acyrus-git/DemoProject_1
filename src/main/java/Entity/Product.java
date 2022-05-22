76845812314cce4c8f3623b0891d8172d99c3f2d
acyrus-git <onlytoadityac@gmail.com>
2022-05-22 10:57:59 +0530

    Added product class

---
+++ src/main/java/Entity/Product.java
@@ -0,0 +1,23 @@
+package Entity;
+
+public class Product {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    private double price;
+}
--- src/main/java/doSomething.java
+++ src/main/java/doSomething.java
@@ -1,5 +1,10 @@
+import Entity.Product;
+
 public class doSomething {
     public static void main(String[] args){
-        System.out.println("Hi there");
+        Product p=new Product();
+        p.setName("Frooti");
+        p.setPrice(9);
+        System.out.println("The price of "+p.getName()+" is "+p.getPrice());
     }
 }
